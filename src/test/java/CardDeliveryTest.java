99fcb870e80296c321c90e9d662e5cd7c083ebb3
Мария <airbelka@bk.ru>
2022-08-28 15:55:49 +0300

    Ошибка в кодировке

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,40 +2,39 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     //нужна логика для даты
     @Test
     void test() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Кострома");
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, 7);
-        SimpleDateFormat rusDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dateMeeting = rusDateFormat.format(calendar.getTime());
-        SelenideElement dateElement = $("[data-test-id=date] input[class=input__control]");
-        dateElement.sendKeys("\b\b\b\b\b\b\b\b\b\b\b\b");
-        dateElement.setValue(dateMeeting);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        String planningDate = generateDate(4);
+        $("[data-test-id='date'] input").val(planningDate);
 
         $("[data-test-id=name] input").setValue("Мария");
         $("[data-test-id=phone] input").setValue("+79103729929");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id=notification]")
-                .$(withText("Встреча успешно забронирована"))
-                .waitUntil(visible, 15000);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
